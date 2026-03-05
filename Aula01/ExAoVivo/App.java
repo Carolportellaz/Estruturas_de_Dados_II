9c38743963d9d603be6aac1a8b9e85f62bc8eaae
Carol <carolportellazanelato@gmail.com>
2026-03-05 06:00:19 -0300

    Update App.java

--- Aula01/ExAoVivo/App.java
+++ Aula01/ExAoVivo/App.java
@@ -4,10 +4,15 @@ public class App {
     static int n;
     static int vet [];
     static int tamanho;
-    static int ultima = -1;
+    static int indice_ultimo = -1;
     static int indice_pai;
     static int soma = 0;
     static int custo = 0;
+    static int ultimo_el = 0;
+    static int pai = 0;
+    static int indice_esquerda = 0;
+    static int indice_direita = 0;
+    static int indice_menor = 0;
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
@@ -24,19 +29,16 @@ public class App {
         System.out.println(custo);
     }
 
-    public static void add(int n){
-        vet[++ultima] = n;
 
-        heapfy(n);
-    }
+    public static void add (int n){
+        vet[++indice_ultimo] = n;
 
-    public static void heapfy(int n){
-        int i = ultima;
+        int i = indice_ultimo;
 
         while(i > 0){
             indice_pai = i / 2;
 
-            if(vet[i] < vet[indice_pai]){
+            if(vet[i] > vet[indice_pai]){
                 int n1 = vet[i];
                 int n2 = vet[indice_pai];
 
@@ -52,15 +54,48 @@ public class App {
         }
     }
 
-    public static void remover(){
-        custo = 0;
-        soma = 0;
+    public static int heapfy(int i){
+        indice_esquerda = 2 * i + 1;
+        indice_direita = 2 * i + 2;
 
-        for(int i = 0; i < vet.length- 1; i++){
-            soma = vet[i] + vet[i + 1];
-
-            vet[i + 1] = soma;
-            custo = custo + soma;
+        if(vet[indice_esquerda] < vet[indice_direita]){
+            indice_menor = indice_esquerda;
         }
+
+        else{
+            indice_menor = indice_direita;
+        }
+
+
+        if(vet[i] < vet[indice_menor]){
+            int n1 = vet[i];
+            int n2 = vet[indice_menor];
+
+            vet[indice_menor] = n1;
+            vet[i] = n2;
+            i = indice_menor;
+
+            return heapfy(i);
+        }
+
+        else{
+            return vet[i];
+        }
+    }
+
+    public static int remover(){
+        ultimo_el = vet[indice_ultimo];
+
+        if(vet[0] != 0){
+            vet[0] = vet[indice_ultimo];
+            vet[indice_ultimo] = 0;
+
+            indice_ultimo--;
+
+            heapfy(0);
+        }
+
+        return vet[0];
+
     }
 }
